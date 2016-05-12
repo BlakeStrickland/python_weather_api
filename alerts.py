afa072dad9d317d6d0f40e39828e9751a6b038f3
Blake Strickland <Stricklandb02@gmail.com>
2016-05-12 09:09:24 -0400

    Current weather alerts

--- alerts.py
+++ alerts.py
@@ -6,5 +6,12 @@ key = os.environ.get('WUNDERGROUND_KEY')
 
 # zip_code = input("Please enter your zip code: ")
 zip_code = 27615
-response = requests.get('http://api.wunderground.com/api/{}/alerts/q/{}.json
-'.format(key, zip_code)).json()
+response = requests.get('http://api.wunderground.com/api/{}/alerts/q/{}.json'.format(key, zip_code)).json()
+
+alert_list = response["alerts"]
+
+if alert_list:
+    for alert in alert_list:
+        print(alert)
+else:
+    print("There are no alerts in your area")
